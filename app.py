a196ad42b65a6db65ab3d0a0d123743d38996297
HaejinYoon <hjyoomp@gmail.com>
2025-10-01 14:14:04 +0900

    카드 헤더 스타일 변경

--- app.py
+++ app.py
@@ -581,7 +581,7 @@ app_ui = ui.page_fluid(
                             #분포 필터
                             ui.div(
                                 f"데이터 분포 그래프 필터",
-                                style="background-color:#e9ecef; padding:8px 12px; border-radius:6px; text-align:center; font-weight:bold;"
+                                style="background-color:#e9ecef; padding:8px 12px; border-radius:6px; text-align:center; font-weight:bold; font-size: 14px"
                             ),
                             ui.input_selectize(
                                 "mold_code2",
@@ -662,7 +662,7 @@ app_ui = ui.page_fluid(
                             ui.card_header("입력 변수", style="background-color:#f8f9fa; text-align:center;"),
                             # 생산 환경 정보 카드 (최상단)
                             ui.card(
-                                ui.card_header("생산 환경 정보", style="background-color:#f8f9fa; text-align:center;"),
+                                ui.card_header("생산 환경 정보", style="text-align:center;"),
                                 ui.layout_columns(
                                     ui.div(
                                         f"생산 라인: {df_raw['line'].iloc[0]}",
@@ -682,7 +682,7 @@ app_ui = ui.page_fluid(
 
                             # === 공정 상태 관련 (4열) ===
                             ui.card(
-                                ui.card_header("공정 상태 관련", style="background-color:#f8f9fa;"),
+                                ui.card_header("공정 상태 관련", style=""),
                                 ui.layout_columns(
                                     ui.input_numeric("count", "일조 누적 제품 개수", value=int(df_predict["count"].mean())),
                                     ui.input_numeric("monthly_count", "월간 누적 제품 개수", value=int(df_predict["monthly_count"].mean())),
@@ -699,7 +699,7 @@ app_ui = ui.page_fluid(
 
                             # === 용융 단계 (n행 4열) ===
                             ui.card(
-                                ui.card_header("용융 단계", style="background-color:#f8f9fa;"),
+                                ui.card_header("용융 단계", style=""),
                                 ui.layout_columns(
                                     make_num_slider("molten_temp"),
                                     make_select("heating_furnace", "용해로"),
@@ -709,7 +709,7 @@ app_ui = ui.page_fluid(
 
                             # === 충진 단계 (n행 4열) ===
                             ui.card(
-                                ui.card_header("충진 단계", style="background-color:#f8f9fa;"),
+                                ui.card_header("충진 단계", style=""),
                                 ui.layout_columns(
                                     make_num_slider("sleeve_temperature"),
                                     make_num_slider("EMS_operation_time"),
@@ -724,7 +724,7 @@ app_ui = ui.page_fluid(
 
                             # === 냉각 단계 (n행 4열) ===
                             ui.card(
-                                ui.card_header("냉각 단계", style="background-color:#f8f9fa;"),
+                                ui.card_header("냉각 단계", style=""),
                                 ui.layout_columns(
                                     make_num_slider("upper_mold_temp1"),
                                     make_num_slider("upper_mold_temp2"),
@@ -739,7 +739,7 @@ app_ui = ui.page_fluid(
 
                             # === 공정 속도 관련 (n행 4열) ===
                             ui.card(
-                                ui.card_header("공정 속도 관련", style="background-color:#f8f9fa;"),
+                                ui.card_header("공정 속도 관련", style=""),
                                 ui.layout_columns(
                                     make_num_slider("facility_operation_cycleTime"),
                                     make_num_slider("production_cycletime"),
@@ -749,7 +749,7 @@ app_ui = ui.page_fluid(
 
                             # === 품질 및 성능 (n행 4열) ===
                             ui.card(
-                                ui.card_header("품질 및 성능", style="background-color:#f8f9fa;"),
+                                ui.card_header("품질 및 성능", style=""),
                                 ui.layout_columns(
                                     make_num_slider("biscuit_thickness"),
                                     make_num_slider("physical_strength"),
@@ -781,7 +781,7 @@ app_ui = ui.page_fluid(
                                     ],
                                     style="display:flex; align-items:center; width:100%;"
                                 ),
-                                style="text-align:center; background-color:#f8f9fa;"
+                                style="background-color:#f8f9fa; text-align:center;" 
                             ),
                             ui.output_ui("prediction_result")
                         ),
@@ -798,7 +798,7 @@ app_ui = ui.page_fluid(
                 ),
                 ui.nav_panel("개선",
                     ui.card(
-                        ui.card_header("불량 기여 요인 Top 5", style="background-color:#f8f9fa; text-align:center;"),
+                        ui.card_header("불량 기여 요인 Top 5", style="text-align:center;"),
                         ui.output_plot("local_factor_plot"),
                         ui.hr(),
                         ui.output_ui("local_factor_desc")   # ← 설명 칸 추가

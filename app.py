85203422da53aa6233450a828b59126361d24689
HaejinYoon <hjyoomp@gmail.com>
2025-10-02 16:04:47 +0900

    부트스트랩 css 제거

--- app.py
+++ app.py
@@ -417,7 +417,6 @@ app_ui = ui.page_fluid(
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         ),
         ui.tags.link(rel="stylesheet", type="text/css", href="custom.css"),
-        # ui.tags.link(rel="stylesheet", type="text/css", href="bootstrap.css"),
         ui.tags.style("""
             /* 날짜 범위 선택 입력창 가로 배치 */
             .input-daterange {
